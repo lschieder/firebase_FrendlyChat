990f2c7f9575ff1c5045044a87581286cd63d789
Laurin Schieder <lschieder@student.tgm.ac.at>
2025-11-28 11:32:46 +0100

    fixing the build process

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,25 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
-import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychatls","appId":"1:943617324950:web:f861616b9020c570248fb7","storageBucket":"friendlychatls.firebasestorage.app","apiKey":"AIzaSyDBXEqlGGkODNWbWodBObi1fZov-E0ITZA","authDomain":"friendlychatls.firebaseapp.com","messagingSenderId":"943617324950","measurementId":"G-ZDDQZM9HYX","projectNumber":"943617324950","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging())
   ],
 };
---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDBXEqlGGkODNWbWodBObi1fZov-E0ITZA",
+  authDomain: "friendlychatls.firebaseapp.com",
+  projectId: "friendlychatls",
+  storageBucket: "friendlychatls.firebasestorage.app",
+  messagingSenderId: "943617324950",
+  appId: "1:943617324950:web:f861616b9020c570248fb7",
+  measurementId: "G-ZDDQZM9HYX"
+};
