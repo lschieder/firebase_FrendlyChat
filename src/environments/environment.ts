cf2ee4425f4fa6511d20220ce8f6f286a1e52ccd
Laurin Schieder <lschieder@student.tgm.ac.at>
2025-12-11 14:55:29 +0100

    fixing apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
+    apiKey: 'AIzaSyDBXEqlGGkODNWbWodBObi1fZov-E0ITZA',
+    authDomain: 'friendlychatls.firebaseapp.com',
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    projectId: 'friendlychatls',
+    storageBucket: 'friendlychatls.firebasestorage.app',
+    messagingSenderId: '943617324950',
+    appId: '1:943617324950:web:f861616b9020c570248fb7',
+    measurementId: 'G-ZDDQZM9HYX',
+
   },
 };
